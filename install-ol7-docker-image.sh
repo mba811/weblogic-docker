e02b1740b720a6304c03e78dbf9e018e18380dad
Bruno Borges <bruno.borges@gmail.com>
2014-12-08 06:06:28 -0800

    delete oraclelinux pkg file

--- install-ol7-docker-image.sh
+++ install-ol7-docker-image.sh
@@ -2,3 +2,4 @@
 curl -O http://public-yum.oracle.com/docker-images/OracleLinux/OL7/oraclelinux-7.0.tar.xz
 unxz oraclelinux-7.0.tar.xz
 sudo docker load -i oraclelinux-7.0.tar
+rm oraclelinux-7.0.tar
