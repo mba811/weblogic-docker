5ad6b00b15f1484c27191d2bb2dcf54b1ae6b98a
Bruno Borges <bruno.borges@gmail.com>
2014-12-08 09:35:41 -0800

    improved dock scripts

--- weblogic12c-developer/dockNodeManager.sh
+++ weblogic12c-developer/dockNodeManager.sh
@@ -1,24 +1,77 @@
 #!/bin/sh
+#
+# Usage:  
+#         dockNodeManager -n [container name running admin server]
+#
+# Since: October, 2014
+# Author: bruno.borges@oracle.com
+# Description: script to create a WLS container based on IMAGE_NAME and start NodeManager within it. After NodeManager is started, a script 'add-machine.py' is called that will automatically add the NodeManager as Machine into the domain associated to ADMIN_CONTAINER_NAME
+#
 IMAGE_NAME="oracle/weblogic-dev:12.1.3"
-if [ ! -e tmp/weblogic.cid ]
-then
-  echo "First start WebLogic Admin Server by calling 'dockWebLogic.sh'"
-  exit
-fi
-
+ADMIN_CONTAINER_NAME=wlsadmin
 TIMESTAMP=`date +%s`
 CONTAINER_NAME=nodemanager${TIMESTAMP}
 CID_FILE=tmp/${CONTAINER_NAME}.cid
 
-docker run -d \
+# CHECK FOR ARGUMENTS
+# -a = will attach port 7001 to host
+# -p = will create tmp/weblogic.cid file to avoid running multiple Admins
+# -n [name] = the name of the admin server container which this NM will automatically plug to. Must exist. Defaults to 'wlsadmin'.
+while getopts "pn:" optname
+  do
+    case "$optname" in
+      "p")
+        USE_PID_CONFIG=true
+        ;;
+      "n")
+        ADMIN_CONTAINER_NAME="$OPTARG"
+        ;;
+      *)
+      # Should not occur
+        echo "Unknown error while processing options"
+        ;;
+    esac
+  done
+
+# CHECK IF CONTAINER EXISTS AND IS RUNNING
+# Based on https://gist.github.com/ekristen/11254304 (MIT Licensed)
+ADMIN_CONTAINER_RUNNING=$(docker inspect --format="{{ .State.Running }}" $ADMIN_CONTAINER_NAME 2> /dev/null)
+ 
+if [ $? -eq 1 ]; then
+  echo "Admin container '$ADMIN_CONTAINER_NAME' with WLS Admin Server running does not exist. Create one first calling 'dockWebLogic.sh -n $ADMIN_CONTAINER_NAME'"
+  exit $? 
+fi
+ 
+if [ "$ADMIN_CONTAINER_RUNNING" == "false" ]; then
+  echo "Admin container '$ADMIN_CONTAINER_NAME' is not running. Unpause or start it"
+  exit $?
+fi
+ 
+GHOST_STATUS=$(docker inspect --format="{{ .State.Ghost }}" $ADMIN_CONTAINER_NAME)
+ 
+if [ "$GHOST_STATUS" == "true" ]; then
+  echo "Admin container '$ADMIN_CONTAINER_NAME' has been ghosted. Destroy it and create again."
+  exit $?
+fi 
+
+# PID FILE
+if [ "$USE_PID_CONFIG" = true ]; then
+  touch tmp/$CID_FILE
+  if [ $? -e 0 ]; then
+    PID_CONFIG="--cidfile $CID_FILE"
+  fi
+fi
+
+# RUN DOCKER
+CONTAINER_ID=`docker run -d \
  --name ${CONTAINER_NAME} \
  -e DOCKER_CONTAINER_NAME=${CONTAINER_NAME} \
- --cidfile $CID_FILE \
- --link wlsadmin:wlsadmin $IMAGE_NAME \
- /u01/oracle/createMachine.sh > /dev/null 2>&1
+ $PID_CONFIG \
+ --link $ADMIN_CONTAINER_NAME:wlsadmin $IMAGE_NAME \
+ /u01/oracle/createMachine.sh > /dev/null 2>&1`
 
-NMIP=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' `cat $CID_FILE`)
+NMIP=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' $CONTAINER_NAME)
 
-echo "New NodeManager [${CONTAINER_NAME}] started on IP Address: ${NMIP}."
-echo "Hopefully this Machine was automatically added to 'base_domain'."
+echo "New NodeManager [$CONTAINER_NAME] started on IP Address: $NMIP."
+echo "Hopefully this Machine was automatically added to 'base_domain' in the [$ADMIN_CONTAINER_NAME] admin server."
 echo "If not, go to Admin Console and try to add it manually with this IP address."
--- weblogic12c-generic/dockWebLogic.sh
+++ weblogic12c-generic/dockWebLogic.sh
@@ -3,35 +3,57 @@ TMP_CID_FILE=tmp/weblogic.cid
 DOCKER_IMAGE_NAME=oracle/weblogic:12.1.3
 DOCKER_CONTAINER_NAME=wlsadmin
 
-if [ ! -e tmp ]
-then
-  mkdir tmp
-fi
+# CHECK FOR ARGUMENTS
+# -a = will attach port 7001 to host
+# -p = will create tmp/weblogic.cid file to avoid running multiple Admins
+# -n [name] = the name of the admin server container. Must be unique. Default to 'wlsadmin'. 
+while getopts "apn:" optname
+  do
+    case "$optname" in
+      "a")
+        ATTACH_DEFAULT_PORT="-p 7001:7001"
+        ;;
+      "p")
+        USE_TMP_PID="true"
+        ;;
+      "n")
+        DOCKER_CONTAINER_NAME="$OPTARG"
+        ;;
+      *)
+      # Should not occur
+        echo "Unknown error while processing options"
+        ;;
+    esac
+  done
 
-if [ -e "$TMP_CID_FILE" ]
-then
-  docker kill `cat $TMP_CID_FILE` > /dev/null 2>&1
-  if [ $? -ne 0 ]
-  then
-    rm -f $TMP_CID_FILE
-  else
-    docker rm `cat $TMP_CID_FILE`  > /dev/null 2>&1 && rm $TMP_CID_FILE
+# CONFIG PID TMP
+if [ "$USE_TMP_PID" = true ]; then
+  if [ ! -e tmp ]; then
+    mkdir tmp
+    if [ "$?" != 0 ]; then
+      echo "Couldn't create tmp dir. Try without PID control (-p)."
+      exit $?
+    fi 
   fi
-fi
 
-if [ ""$1 = "-attach" ]
-then
-  ATTACH_DEFAULT_PORT="-p 7001:7001"
+  if [ -e "$TMP_CID_FILE" ]; then
+    docker kill `cat $TMP_CID_FILE` > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+      rm -f $TMP_CID_FILE
+    else
+      docker rm `cat $TMP_CID_FILE`  > /dev/null 2>&1 && rm $TMP_CID_FILE
+    fi
+  fi
+
+  PID_CONFIG="--cidfile $TMP_CID_FILE"
 fi
 
 # RUN THE DOCKER COMMAND
-
 docker run \
  -d $ATTACH_DEFAULT_PORT \
- --cidfile $TMP_CID_FILE \
+ $PID_CONFIG \
  --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME \
- /u01/oracle/weblogic/user_projects/domains/base_domain/startWebLogic.sh 
-# > /dev/null 2>&1
+ /u01/oracle/weblogic/user_projects/domains/base_domain/startWebLogic.sh
 
 # EXTRACT THE IP ADDRESS
 if [ -n "${ATTACH_DEFAULT_PORT}" ]
@@ -41,6 +63,12 @@ else
   WLS_ADMIN_IP=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' $DOCKER_CONTAINER_NAME)
 fi
 
-echo "WebLogic starting... "
-sleep 10
-echo "Open WebLogic Console at http://${WLS_ADMIN_IP}:7001/console"
+# REPORT IF DOCKER SUCCEEDED
+if [ "$?" = 0 ]; then
+  echo "WebLogic starting... "
+  sleep 10
+  echo "Open WebLogic Console at http://${WLS_ADMIN_IP}:7001/console"
+else
+  echo "There was an error trying to create a container"
+  exit $?
+fi
