994b24b3fae1f03e5fb0961b9cdc214e6b1822b1
Bruno Borges <bruno.borges@oracle.com>
2014-12-25 15:20:47 -0200

    generic dockerfile does not create a domain; use the container-domain DOckerfile as a starting point

--- bin/setDockerEnv.sh
+++ bin/setDockerEnv.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 DISTRIBUTION="weblogic12c-generic"
 SKIP_BASE_DOMAIN_CREATION=false
-IMAGE_NAME="oracle/weblogic:12.1.3"
+
+if [ -z "$IMAGE_NAME" ]; then
+	IMAGE_NAME="oracle/weblogic:12.1.3"
+fi
+
 TIMESTAMP=`date +%s`
 NM_CONTAINER_NAME="nodemanager${TIMESTAMP}"
 ADMIN_CONTAINER_NAME="wlsadmin"
@@ -17,6 +21,7 @@ WLS_PKG="fmw_12.1.3.0.0_wls.jar"
 WLS_PKG_MD5="8378fe936b476a6f4ca5efa465a435e3"
 
 setup_developer() {
+	IMAGE_NAME="oracle/weblogic:12.1.3-dev"
 	echo "Builder configured to build developer image"
         DISTRIBUTION="weblogic12c-developer"
         WLS_PKG="wls1213_dev.zip"
